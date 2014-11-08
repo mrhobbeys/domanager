e59a8b120d3b7787ea8b1962b7da83bfcbbf35e3
Artur <nobu@aoizora.org>
2014-11-08 19:47:35 +0500

    #14: Mac OS X build scripts updated

--- build_mac.py
+++ build_mac.py
@@ -11,53 +11,28 @@ from domanager.config import config
 title = "DO_Manager"
 appName = title
 sourceDir = os.path.join("dist", "%s.app" % title)
-backImgDir = os.path.join("/Volumes/%s" % title, ".background")
 approxSize = "70048k"
 dmgPath = os.path.join("dist", "domanager.dmg")
 version = config.version
 finalDmgPath = os.path.join("dist", "%s_%s.dmg" % (title, version))
-excludeFrameworks = ['QtMultimedia.framework', 'QtNetwork.framework', 'QtSql.framework',
-                     'QtSvg.framework', 'QtXml.framework', 'QtXmlPatterns.framework',
-                     'QtScript.framework', 'QtScriptTools.framework', 'QtDeclarative.framework']
+
+tempPath = os.path.join("dist", "temp")
+
+pkgName = "Install DO Manager.pkg"
+pkgPath = os.path.join(tempPath, pkgName)
 
 # Commands
 buildApp = [sys.executable, "build_pkg_mac.py",
             "py2app", "-O2", "--iconfile", "source/domanager/resources/main_logo_color.icns"]
 
-copyBackImg = ["cp", os.path.join("source", "domanager", "resources", "back.png"), backImgDir]
-
 createDmg = " ".join(["hdiutil", "create", "-srcfolder",
-                      '"%s"' % sourceDir, "-volname", '"%s"' % title,
+                      '"%s"' % tempPath, "-volname", '"%s"' % title,
                       "-fs", "HFS+", "-fsargs", '"-c c=64,a=16,e=16"',
                       "-format", "UDRW", "-size", approxSize, dmgPath])
 
 getDevice = " ".join(["hdiutil", "attach", "-readwrite", "-noverify",
                       "-noautoopen", dmgPath, "|", "egrep", "'^/dev/'", "|",
                       "sed", "1q", "|", "awk", "'{print $1}'"])
-
-setVisualStyle = """echo '
-                    tell application "Finder"
-                        tell disk "'%s'"
-                            open
-                            set current view of container window to icon view
-                            set toolbar visible of container window to false
-                            set statusbar visible of container window to false
-                            set the bounds of container window to {400, 100, 885, 350}
-                            set theViewOptions to the icon view options of container window
-                            set arrangement of theViewOptions to not arranged
-                            set icon size of theViewOptions to 72
-                            set background picture of theViewOptions to file ".background:'back.png'"
-                            set position of item "'%s'" of container window to {100, 100}
-                            make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
-                            set position of item "Applications" of container window to {375, 100}
-                            close
-                            open
-                            update without registering applications
-                            delay 5
-                            eject
-                        end tell
-                    end tell
-                    ' | osascript""" % (title, appName)
 
 compressRelease = """chmod -Rf go-w /Volumes/"%s"
                      sync
@@ -67,33 +42,33 @@ compressRelease = """chmod -Rf go-w /Volumes/"%s"
                      hdiutil convert "%s" -format UDZO -imagekey zlib-level=9 -o "%s"
                      rm -f %s"""
 
+createAppPkg = "pkgbuild --install-location /Applications --component '%s' '%s'" % (sourceDir, pkgPath)
+
 # Actual execution
 sys.path.append(os.path.abspath("."))
 
-check_call (buildApp)
+print 'Building application...'
+check_call(buildApp)
 
-qtConf = "; Qt Configuration file\n[Paths]\nPlugins = Resources/qt_plugins"
-qtConfDir = os.path.join(sourceDir, "Contents", "Resources")
-f = open(os.path.join(qtConfDir, "qt.conf"), "wb")
-f.write(qtConf)
-f.close()
+if not os.path.exists(tempPath):
+    os.makedirs(tempPath)
 
-for frName in excludeFrameworks:
-    frPath = os.path.join(sourceDir, "Contents", "Frameworks", frName)
-    if os.path.exists(frPath):
-        shutil.rmtree(frPath)
+print 'Creating PKG file...'
+check_call(createAppPkg, shell=True)
 
-check_call (createDmg, shell=True)
+print 'Creating DMG file...'
+check_call(createDmg, shell=True)
 
 result = Popen(getDevice, stdout=PIPE, shell=True)
 deviceName = result.communicate()[0].replace('\n','')
 
 sleep(5)
 
-os.makedirs(backImgDir)
-check_call (copyBackImg)
-
-check_call (setVisualStyle, shell=True)
-
+print 'Compressing DMG...'
 command = compressRelease % (title, deviceName, dmgPath, finalDmgPath, dmgPath)
-check_call (command, shell=True)
+check_call(command, shell=True)
+
+print 'Cleaning up...'
+shutil.rmtree("../build", ignore_errors=True)
+shutil.rmtree(tempPath, ignore_errors=True)
+shutil.rmtree(sourceDir, ignore_errors=True)
--- build_pkg_mac.py
+++ build_pkg_mac.py
@@ -15,18 +15,26 @@ DATA_FILES = [os.path.join("source", "domanager", "resources")]
 OPTIONS = {'argv_emulation': True,
            'iconfile': os.path.join("source", "domanager",
                                     "resources", "main_logo_color.icns"),
-           
-           "qt_plugins": ["imageformats/*"], 
 
-           "excludes": ['PyQt4.QtDesigner', 'PyQt4.QtNetwork', 'PyQt4.QtOpenGL', 'PyQt4.QtScript', 
-                        'PyQt4.QtSql', 'PyQt4.QtTest', 'PyQt4.QtWebKit', 'PyQt4.QtXml', 'PyQt4.phonon',
-                        '_gtkagg', '_tkagg', 'bsddb', 'curses', 'pywin.debugger',
-                        'pywin.debugger.dbgcon', 'pywin.dialogs',  'tcl', 'test', 'Tkconstants',
-                        'Tkinter', 'xml', 'pywinauto.tests', 'unittest',
-                        'pdb', 'dummy_thread', 'doctest', 'PIL',
-                        'BmpImagePlugin', 'GifImagePlugin', 'GimpGradientFile', 
-                        'GimpPaletteFile', 'JpegImagePlugin', 'PngImagePlugin', 'PpmImagePlugin', 
-                        'TiffImagePlugin', 'TiffTags', 'Image', 'ImageGrab', 'bz2', 'email' ],
+           'includes': ["domanager", "sip", "PyQt5.QtCore", "PyQt5.QtWidgets", "PyQt5.QtGui"],
+
+           "qt_plugins": ["imageformats/*", "platforms/*"],
+
+           "excludes": ["numpy", "sqlalchemy", 'h5py', 'cx_Freeze', 'coverage',
+                        'Enginio', 'PyQt5.QtBluetooth', 'PyQt5.QtHelp', 'PyQt5.QtMultimediaWidgets',
+                        'PyQt5.QtPositioning', 'PyQt5.QtQml', 'PyQt5.QtQuick', 'PyQt5.QtQuickWidgets',
+                        'PyQt5.QtSensors', 'PyQt5.QtSerialPort', 'PyQt5.QtWebKitWidgets', 'bs4', 'altgraph',
+                        'PyQt5.QtDesigner', 'PyQt5.QtMultimedia', 'PyQt5.QtOpenGL', 'jinja2', 'email',
+                        'PyQt5.QtSvg', 'PyQt5.QtSql', 'PyQt5.QtXml', 'PyQt5.QtXmlPatterns', ''
+                        'PyQt5.QtWebKit', 'PyQt5.QtTest', 'PyQt5.QtScript', 'PyQt5.QtScriptTools',
+                        'PyQt5.QtNetwork', 'PyQt5.QtDeclarative', 'PyQt5.QtWebSockets', 'modulegraph',
+                        '_gtkagg', '_tkagg', 'bsddb', 'curses', 'pywin.debugger', 'py2app', 'mercurial'
+                        'pywin.debugger.dbgcon', 'pywin.dialogs',  'tcl', 'test', 'ply', 'nose',
+                        'Tkinter', 'xml', 'pywinauto.tests', 'unittest', 'Tkconstants', 'pyface',
+                        'pdb', 'dummy_thread', 'doctest', 'PIL', 'PpmImagePlugin', 'traitsui',
+                        'BmpImagePlugin', 'GifImagePlugin', 'GimpGradientFile', 'tornado', 'traits',
+                        'GimpPaletteFile', 'JpegImagePlugin', 'PngImagePlugin', 'xlrd', 'xlwt',
+                        'TiffImagePlugin', 'TiffTags', 'Image', 'ImageGrab', 'bz2'],
 
             'plist': {'LSUIElement': True},
             }
